d9e4bca51c42e491ff168bc1b52cfc51af05f1cd
maxrene <maxrene@google.com>
2025-11-22 10:22:27 +0000

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,6 @@
 
 const BASE_URL = 'https://api.themoviedb.org/3';
 const IMG_BASE_POSTER = 'https://image.tmdb.org/t/p/w500';
-const TMDB_API_KEY = 'f1b07b5d2ac7a9f55c5b49a93b18bd33';
 
 document.addEventListener('DOMContentLoaded', () => {
     // Containers
