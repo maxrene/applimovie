27d15b067d88316fed9e98a653a27c551dc9fd77
maxrene <maxrene@google.com>
2026-04-02 18:28:05 +0100

    Update popular.js

--- js/popular.js
+++ js/popular.js
@@ -16,9 +16,9 @@ document.addEventListener('alpine:init', () => {
         // Platform Mapping
         allPlatforms: [
             { id: 'netflix', apiId: 8, name: 'Netflix', logoUrl: 'https://images.ctfassets.net/4cd45et68cgf/Rx83JoRDMkYNlMC9MKzcB/2b14d5a59fc3937afd3f03191e19502d/Netflix-Symbol.png?w=700&h=456' },
-            { id: 'prime', apiId: 119, name: 'Prime Video', logoUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/ca/Amazon_Prime_Video_logo_%282024%29.svg/1024px-Amazon_Prime_Video_logo_%282024%29.svg.png' },
+            { id: 'prime', apiId: 119, name: 'Prime Video', logoUrl: 'https://logo.clearbit.com/primevideo.com' }, // <-- MODIFIÉ
             { id: 'disney', apiId: 337, name: 'Disney+', logoUrl: 'https://platform.theverge.com/wp-content/uploads/sites/2/chorus/uploads/chorus_asset/file/25357066/Disney__Logo_March_2024.png?quality=90&strip=all&crop=0,0,100,100' },
-            { id: 'apple', apiId: 350, name: 'Apple TV+', logoUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ad/AppleTVLogo.svg/768px-AppleTVLogo.svg.png' },
+            { id: 'apple', apiId: 350, name: 'Apple TV+', logoUrl: 'https://logo.clearbit.com/tv.apple.com' }, // <-- MODIFIÉ
             { id: 'canal', apiId: 392, name: 'Canal+', logoUrl: 'https://static1.purepeople.com/articles/0/46/23/10/@/6655765-logo-de-la-chaine-canal-1200x0-2.png' },
             { id: 'paramount', apiId: 531, name: 'Paramount+', logoUrl: 'https://images.seeklogo.com/logo-png/39/1/paramount-logo-png_seeklogo-397501.png' },
             { id: 'max', apiId: 1899, name: 'Max', logoUrl: 'https://logo.clearbit.com/max.com' },
